b52ffa807f139393b1fe92ff8ebeaa5bfa36391e
muroa96 <thsgskwhd@gmail.com>
2022-07-20 19:19:08 +0900

    2022-07-20

---
+++ practice.js
@@ -0,0 +1,14 @@
+let arr = ['a','b','c'];
+let upperarr = [];
+arr.forEach(function(elements) {
+  upp = elements.toUpperCase();
+  upperarr.push(upp);
+});
+console.log(upperarr);
+
+let arr1 = ['a', 'b'];
+let upperarr2 = ;
+
+arr1.forEach(function(el){
+  upperarr2.push
+});
